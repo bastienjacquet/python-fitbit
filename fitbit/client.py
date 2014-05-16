fcf137fa7e15bb095f8f82e5c490541bd91f4417
dcharlton <dcharlton@apple-pi.(none)>
2014-05-16 14:13:43 +0000

    Add support for activity log records and minute resolution activity log data

--- fitbit/client.py
+++ fitbit/client.py
@@ -25,6 +25,9 @@ try:
 except ImportError:
     import http.cookiejar as cookielib
 
+import dateutil.parser
+import json
+
 _log = logging.getLogger("fitbit")
 
 class Client(object):
@@ -67,15 +70,81 @@ class Client(object):
         or you will just get the first sleep of the day
         """
         return self._graphdata_intraday_sleep_request("intradaySleep", date, sleep_id=sleep_id)
-    
-    def _request(self, path, parameters):
-        # Throw out parameters where the value is not None
-        parameters = dict([(k,v) for k,v in parameters.items() if v])
-        
-        query_str = urlencode(parameters)
 
-        request = Request("%s%s?%s" % (self.url_base, path, query_str))
-        _log.debug("requesting: %s", request.get_full_url())
+    def activity_logs(self,date):
+        """Retrieve the available activity logs for the given date
+        the format is: [(id,datetime.datetime,name,steps,distance,duration,calories), ...]
+        """
+        json_data=self._api2request("GET /api/2/user/activities/logs","user","getActivitiesLogs",{"fromDate":str(date),"toDate":str(date),"period":"day","offset":0,"limit":10})
+        values = [Client._marshall_activity_log(log) for log in json_data]
+        return values
+
+    def activity_log_data_calories(self,date,id):
+        """Retrieve the calories burned data in 1 minute intervals for the given date and activity log
+        the format is: [(datetime.datetime,calories), ...]
+        You need to call activity_logs() first to find the ids for activity log entries
+        """
+        return self._activity_log_data(date,id,'activityRecordCaloriesBurned')
+
+    def activity_log_data_steps(self,date,id):
+        """Retrieve the steps data in 1 minute intervals for the given date and activity log
+        the format is: [(datetime.datetime,steps), ...]
+        You need to call activity_logs() first to find the ids for activity log entries
+        """
+        return self._activity_log_data(date,id,'activityRecordSteps')
+
+    def activity_log_data_floors(self,date,id):
+        """Retrieve the steps floors climbed in 1 minute intervals for the given date and activity log
+        the format is: [(datetime.datetime,floors), ...]
+        You need to call activity_logs() first to find the ids for activity log entries
+        """
+        return self._activity_log_data(date,id,'activityRecordFloors')
+
+    def activity_log_data_pace(self,date,id):
+        """Retrieve the pace data in 1 minute intervals for the given date and activity log
+        the format is: [(datetime.datetime,pace), ...]
+        You need to call activity_logs() first to find the ids for activity log entries
+        """
+        return self._activity_log_data(date,id,'activityRecordPace')
+
+    def activity_log_data_all(self,date,id):
+        """Retrieve the activity data in 1 minute intervals for the given date and activity log
+        the format is: [(datetime.datetime,calories,steps,floors,pace), ...]
+        You need to call activity_logs() first to find the ids for activity log entries
+        """
+        calories=self.activity_log_data_calories(date,id)
+        steps=self.activity_log_data_steps(date,id)
+        floors=self.activity_log_data_floors(date,id)
+        pace=self.activity_log_data_pace(date,id)
+
+        # assumes len(calories)==len(steps) etc - which is probably reasonable
+
+        return zip(
+            [ l[0] for l in calories], 
+            [ l[1] for l in calories],
+            [ l[1] for l in steps],
+            [ l[1] for l in floors],
+            [ l[1] for l in pace])
+
+    def _request(self, path, parameters):
+        data=self._request_raw(path, parameters)
+        return ET.fromstring(data.strip().replace("&hellip;", "..."))
+
+    def _request_json(self, path, post_data,method):
+        data=self._request_raw(path, post_data,method)
+        return json.loads(data)
+
+    def _request_raw(self, path, parameters,method="GET"):
+
+        if method == "POST":
+            request = Request("%s%s" % (self.url_base, path),parameters)
+        else:    
+            # Throw out parameters where the value is not None
+            parameters = dict([(k,v) for k,v in parameters.items() if v])
+            query_str = urlencode(parameters)
+            request = Request("%s%s?%s" % (self.url_base, path, query_str))
+        
+        _log.debug("requesting (%s): %s", method,request.get_full_url())
 
         data = None
         try:
@@ -88,8 +157,8 @@ class Client(object):
 
         #_log.debug("response: %s", data)
 
-        return ET.fromstring(data.strip().replace("&hellip;", "..."))
-
+        return data
+  
     def _graphdata_intraday_xml_request(self, graph_type, date, data_version=2108, **kwargs):
         params = dict(
             userId=self.user_id,
@@ -146,6 +215,85 @@ class Client(object):
         values = [int(float(v.text)) for v in xml.findall("data/chart/graphs/graph/value")]
         return zip(datetimes, values)
     
+    def _api2request(self,id,name,method,args):
+
+        c=json.dumps({"serviceCalls":
+                    [
+                    {
+                        "id":id,
+                        "name":name,
+                        "method":method,
+                        "args":args
+                    }
+                    ],"template":"activities/modules/models/ajax.response.json.jsp"},
+                separators=(',',':')
+                )
+
+        api_data = urllib.urlencode(
+            {   'request' : 
+                c})     
+
+        json_data =  self._request_json('/ajaxapi', api_data,"POST")
+
+        for service,result in json_data.items():
+            status=result.get('status',None)
+            value=result.get('result',None)
+
+            if (status==200 and result!=None):
+                #print service
+                return value
+
+    def _activity_log_data(self,date,id,chart_type):
+        json_data=self._newgraph_json_request(chart_type,date,id)
+
+        values=[]
+
+        if ('graph' in json_data):
+            data_points=json_data['graph']['dataSets']['activity']['dataPoints']
+
+            values=[(
+                dateutil.parser.parse(data_point['dateTime']),
+                int(data_point['value'])) 
+                for data_point in data_points]
+
+        return values        
+
+    def _newgraph_json_request(self,graph_type,date,arg):
+        params = dict(
+            userId=self.user_id,
+            type=graph_type,
+            dateFrom=str(date),
+            dateTo=str(date),
+            arg=arg,
+            apiFormat='json'
+        )
+        json_data=self._request_json("/graph/getNewGraphData", params,"GET")
+        return json_data                
+
+    @staticmethod
+    def _marshall_activity_log(log):
+        
+        #_log.debug("log json: %s", log)
+
+        timestamp=dateutil.parser.parse(log['dateTime'])
+        id=log['id']
+        steps=int(log['steps']) if log['steps'] else 0
+        calories=int(log['calories']) if log['calories'] else 0
+
+        name=log['name'] if log['name'] else log['defaultName']
+
+        match = re.match(r"([0-9\.]+) +(.*)", log['formattedDistance'])
+        distance=float(match.group(1))
+        unit=match.group(2)
+        distance=distance*1.609344  if unit=='miles' else distance
+
+        parts=re.split(r":",log["formattedDuration"])        
+        while len(parts)<3:
+            parts.insert(0,0)
+        duration=datetime.timedelta(hours=int(parts[0]), minutes=int(parts[1]), seconds=int(parts[2]))
+
+        return (id,timestamp,name,steps,distance,duration,calories)
+
     @staticmethod
     def login(email, password, base_url="https://www.fitbit.com"):
         cj = cookielib.CookieJar()
